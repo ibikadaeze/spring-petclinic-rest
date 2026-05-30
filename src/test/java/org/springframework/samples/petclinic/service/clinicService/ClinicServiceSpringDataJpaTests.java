2b63a931b6a47182bf92ce8dc3a683e7b9477dc6
Your Name <your.email@company.com>
2026-05-30 14:19:29 -0400

    Fix user creation endpoint mapping4

--- src/test/java/org/springframework/samples/petclinic/service/clinicService/ClinicServiceSpringDataJpaTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/clinicService/ClinicServiceSpringDataJpaTests.java
@@ -6,14 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
-/**
- * <p> Integration test using the 'Spring Data' profile.
- *
- * @author Michael Isvy
- * @see AbstractClinicServiceTests AbstractClinicServiceTests for more details. </p>
- */
 @SpringBootTest
-@ActiveProfiles({"spring-data-jpa", "hsqldb"}) // Moved this up above the class
+@ActiveProfiles({"spring-data-jpa", "hsqldb"})
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class ClinicServiceSpringDataJpaTests extends AbstractClinicServiceTests {
 
